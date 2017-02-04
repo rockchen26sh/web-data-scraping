ae011703978e6adaf0b41f83b16c3876ccd0a3bd
rockchen26sh <cck8787878@gmail.com>
2017-02-04 16:50:19 +0800

    170204

--- 5.4.py
+++ 5.4.py
@@ -1,1 +1,10 @@
-import
+import pymysql
+
+conn = pymysql.connect(host = '127.0.0.1',user = 'root', passwd = '28488747' )
+cur = conn.cursor()
+cur.execute("USE scraping")
+
+cur.execute("SELECT * FROM pages WHERE id=1")
+print(cur.fetchone())
+cur.close()
+conn.close()
---
+++ 5.5.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+import pymysql
+
+conn = pymysql.connect(host = '127.0.0.1',user = 'root', passwd = '28488747' )
+cur = conn.cursor()
+cur.execute("USE scraping")
+
+random.seed(datetime.datetime.now())
+def store(title,content):
+    cur.execute("INSERT INTO pages (title,content) VALUE (\"%s\",\"%s\")",(title,content))
+    cur.connection.commit()
+
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bsObj = BeautifulSoup(html)
+    title = bsObj.find("h1").get_text()
+    content = bsObj.find("div",{"id":"mw-content-text"}).find("p").find("b").get_text()
+    store(title,content)
+    return bsObj.find("div",{"id":"bodyContent"}).findAll("a",href = re.compile("^(/wiki/)((?!:).)*$"))
+
+links = getLinks("/wiki/Kevin_Bacon")
+try:
+    while len(links) > 0:
+        newArticle =links[random.randint(0,len(links)-1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
+finally:
+    cur.close()
+    conn.close()
